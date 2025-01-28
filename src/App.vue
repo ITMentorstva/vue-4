3fccf0cd256554b20065aeca28d04e246f897d4e
FasterThanIE <tomicawarr@gmail.com>
2025-01-28 09:18:19 +0100

    * Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Hello world from Main!
+  <Products></Products>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Products from "@/components/Products.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Products
   }
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+</script>
---
+++ src/components/Products.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <div v-if="!apiError">
+    <p v-if="isLoading">Loading products...</p>
+    <p v-if="!isLoading && !fetchedProducts.length">Nemamo proizvode za tu kategoriju</p>
+  </div>
+
+  <p v-if="apiError">{{ apiError }}</p>
+
+  <ul v-if="!isLoading && fetchedProducts.length">
+    <li v-for="product in fetchedProducts" :key="product.id">
+      {{ product.title }} - ${{ product.price }} - Stock: {{ product.stock }}
+    </li>
+  </ul>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+
+  name: 'Products',
+
+  data() {
+    return {
+      fetchedProducts: [],
+      apiError: null,
+      isLoading: false,
+    }
+  },
+
+  beforeCreate() {
+    console.log("Before create")
+  },
+
+  created() {
+    console.log("Created")
+  },
+
+  beforeMount() {
+    console.log('Before mount')
+  },
+
+  mounted() {
+
+    this.isLoading = true;
+
+    axios.get('https://dummyjson.com/products/search?q=phone')
+        .then(response => this.fetchedProducts = response.data.products)
+        .catch(() => this.apiError = "Failed to load products. Please try again later.")
+        .finally(() => this.isLoading = false)
+  }
+}
+</script>
