a05a02f4c6bea74ceba1edd145d7208e3d323c8e
FasterThanIE <tomicawarr@gmail.com>
2025-01-28 10:56:14 +0100

    * added single product

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import ProductsView from "@/views/ProductsView.vue";
+import SingleProductView from "@/views/SingleProductView.vue";
 
 // http://localhost:8080/ -> /
 // http://localhost:8080/products -> /products
@@ -12,7 +13,8 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', name: 'Home', component: HomeView}, // http://localhost:8080/
-        {path: '/products', name: 'Products', component: ProductsView}
+        {path: '/products', name: 'Products', component: ProductsView},
+        {path: '/product/:id', name: 'SingleProduct', component: SingleProductView},
     ],
 });
 
